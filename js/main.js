11a7be5dc4771f0650c308bbef5b630685ebed5b
hyacinta <kimkyuree84@gmail.com>
2021-07-11 17:06:58 +0900

    메뉴 이미지 찌그러짐 해결, 메뉴 클릭 기본 동작 취소

--- js/main.js
+++ js/main.js
@@ -1,26 +1,27 @@
 // 메인
-const menuItem = document.querySelector('.main__menuItem');
-const menuDetail = document.querySelector('.main__menuDetail');
+const menuItem = document.querySelector(".main__menuItem");
+const menuDetail = document.querySelector(".main__menuDetail");
 
-const menuItems= document.querySelectorAll('.main__menuItem');
-const menuDetails = document.querySelectorAll('.main__menuDetail');
-const closeBtns = document.querySelectorAll('.menuDetail__btnClosed')
+const menuItems = document.querySelectorAll(".main__menuItem");
+const menuDetails = document.querySelectorAll(".main__menuDetail");
+const closeBtns = document.querySelectorAll(".menuDetail__btnClosed");
 
-
-for(let i = 0; i < menuItems.length; i++){
-    let items = menuItems[i];
-    items.addEventListener('click', detailUp);    
+for (let i = 0; i < menuItems.length; i++) {
+  let items = menuItems[i];
+  items.addEventListener("click", detailUp);
 }
 
 function detailUp(e) {
-    let _this = e.target;
-    let _parent = _this.closest('li');
-    let _one = _parent.querySelector('.main__menuDetail');
-    _one.classList.add('main__menuDetail--active');
+  e.preventDefault();
+  let _this = e.target;
+  let _parent = _this.closest("li");
+  let _one = _parent.querySelector(".main__menuDetail");
+  _one.classList.add("main__menuDetail--active");
 }
 
-for(let i = 0; i < closeBtns.length; i++){
-    let btns = closeBtns[i];
-    // console.log(menuDetails[i]);
-    btns.addEventListener('click', function() {menuDetails[i].classList.remove('main__menuDetail--active')});
+for (let i = 0; i < closeBtns.length; i++) {
+  let btns = closeBtns[i];
+  btns.addEventListener("click", function (e) {
+    menuDetails[i].classList.remove("main__menuDetail--active");
+  });
 }
